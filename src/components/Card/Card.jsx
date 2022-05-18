00bf4879303526c82ffe802cc8d6e9be400ab532
Luis Simosa <simosa37@gmail.com>
2022-05-18 20:47:04 -0300

    upd: manifest & index.html

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,18 +7,28 @@ const Card = ({
   },
   data,
 }) => {
+  
+// funcion para recortar string
+  
+  
   return (
     <div className="card_container">
       <h3>{title}</h3>
       <div className="card_img_container">
         <img src={data.volumeInfo.imageLinks.thumbnail} alt="Book cover" />
       </div>
-      <p>{subtitle}</p>
+      <p>{(subtitle)}</p>
       <p>{publishedDate}</p>
       <p className="card_author">{authors ? authors : "No registra autores"}</p>
       <div className="card_btns_container">
         <a>Ver más</a>
-        <a href={data.volumeInfo.previewLink} target="_blank" rel="noopener noreferrer">Leer</a>
+        <a
+          href={data.volumeInfo.previewLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Leer
+        </a>
       </div>
     </div>
   );

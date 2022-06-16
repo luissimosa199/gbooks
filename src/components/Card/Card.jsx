027f0ba5f6e9c6c0ff875451dcab4fa21a36c5f1
Luis Simosa <luissimosaarg@gmail.com>
2022-06-16 13:45:24 -0300

    style: add material ui to card design

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,45 +1,76 @@
-import "./Card.style.css";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const Card = ({
+const Cards = ({
   data: {
     id,
     volumeInfo: { title, subtitle, authors, publishedDate },
   },
   data,
 }) => {
-
   // funcion para recortar string
-  const limitString = (str) => {
-    if (str.length > 170) {
-      return  str.slice(0, 167).concat("...");
+  const limitString = (str, n) => {
+    if (str.length > n) {
+      return str.slice(0, n - 3).concat("...");
     }
 
     return str;
   };
 
   return (
-    <div className="card_container">
-      <h3>{title}</h3>
-      <div className="card_img_container">
-        <img src={data.volumeInfo.imageLinks.thumbnail} alt="Book cover" />
-      </div>
-      <p>
-        {subtitle && limitString(subtitle)}
-      </p>
-      <p>{publishedDate}</p>
-      <p className="card_author">{authors ? authors : "No registra autores"}</p>
-      <div className="card_btns_container">
-        <a>Ver más</a>
-        <a
+    <Card
+      sx={{
+        width: 350,
+        minHeight: "65vh",
+        marginY: 2,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+      <CardMedia
+        sx={{ objectFit: "contain", marginTop: 1 }}
+        component='img'
+        alt='book cover'
+        height='199'
+        width='128'
+        image={data.volumeInfo.imageLinks.thumbnail}
+      />
+
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='h3'>
+          {limitString(title, 100)}
+        </Typography>
+
+        <Typography variant='body2' color='text.secondary'>
+          {subtitle && limitString(subtitle, 170)}
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          {publishedDate} -{authors ? authors : "No registra autores"}
+        </Typography>
+      </CardContent>
+
+      <CardActions sx={{width: "100%", display: "flex", justifyContent: "space-around"}}>
+        <Button size='large' variant='outlined' sx={{width: "49%"}}>
+          Ver más
+        </Button>
+        <Button
+          size='large'
+          variant='outlined'
           href={data.volumeInfo.previewLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          target='_blank'
+          rel='noopener noreferrer'
+          sx={{width: "49%"}}
+          >
           Leer
-        </a>
-      </div>
-    </div>
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
-export default Card;
+export default Cards;

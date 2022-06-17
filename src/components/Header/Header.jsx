a11c323b4ace029a0b2cc04f68f5ec6507dc096a
Luis Simosa <luissimosaarg@gmail.com>
2022-06-17 11:36:13 -0300

    style: header typography

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,17 @@ import { useSelector } from "react-redux";
 import Search from "../Search/Search";
 import './Header.style.css';
 
+import Typography from "@mui/material/Typography";
+
 const Header = () => {
 
     const { url: { term }, totalItems } = useSelector(state => state.results)
 
   return (
     <header>
-      <h1>Gbooks</h1>
+      <Typography variant="h2" component="h1" >Gbooks</Typography>
       <Search/>
-      <h2>Resultados para: {term}</h2>
+      <Typography sx={{ fontSize: "1.2em", }} variant="body1" gutterBottom={false}>Resultados para: {term}</Typography>
       <p>Total de resultados: {totalItems}</p>
     </header>
   );
